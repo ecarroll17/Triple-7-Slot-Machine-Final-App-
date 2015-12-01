88c7dcdc393ac580aaf1667d57f6fa661b1922ff
ecarroll <student@ecarroll.local>
2015-12-01 11:01:54 -0600

    Initial Commit

---
+++ Final App - Slot Machine/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Final App - Slot Machine
+//
+//  Created by ecarroll on 12/1/15.
+//  Copyright © 2015 ecarroll. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Final App - Slot MachineTests/Final_App___Slot_MachineTests.swift
@@ -0,0 +1,36 @@
+//
+//  Final_App___Slot_MachineTests.swift
+//  Final App - Slot MachineTests
+//
+//  Created by ecarroll on 12/1/15.
+//  Copyright © 2015 ecarroll. All rights reserved.
+//
+
+import XCTest
+@testable import Final_App___Slot_Machine
+
+class Final_App___Slot_MachineTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
